3bc4a65eb3c9a5e4ece4e5364fb62e2855ff98a8
dikdableu <dikdableu@hotmail.fr>
2022-06-29 01:45:16 +0200

    puppeteer core

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 const client = new Discord.Client();
 const prefix = "!";
