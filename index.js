78f9911ff3f11ffd0e201929bae69459dfa1e0b2
dikdableu <dikdableu@hotmail.fr>
2022-06-29 03:27:26 +0200

    test

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ client.on("message", function(message) {
                     const page = await browser.newPage();
                     await page.goto('https://nomanssky.fandom.com/fr/wiki/Recettes_de_raffinerie');
                     let urls1 = await page.evaluate(() => {
+                        console.log('test')
                         headings_elements_table1 = document.querySelectorAll('table tbody tr')
                         console.log('debug')
                         headings_array_table1 = Array.from(headings_elements_table1); 
